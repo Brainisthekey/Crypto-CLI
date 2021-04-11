54dca3364952c82671a76aa0d65abe7c85e5be7c
Andrii Alieksieiev <andriialieksieiev@gmail.com>
2021-04-11 11:03:26 +0200

    feat: create __init__, super().__init__

--- CryptoExchangeProcessor/binance_exchange_processor.py
+++ CryptoExchangeProcessor/binance_exchange_processor.py
@@ -1,7 +1,12 @@
 from exchange_processor import CryptoExchangeProcessor
+from HTTPclient.binance_client import BinanceClient
 
 class BinanceExchangeProcessor(CryptoExchangeProcessor):
 
+    def __init__(self, client=BinanceClient):
+        super().__init__(client)
+        self.path_to_time = '/time'
+        self.client = client
 
     def get_server_time(self):
         pass
