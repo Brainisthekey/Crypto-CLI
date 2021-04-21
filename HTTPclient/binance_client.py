a4662a24e580c787c11f0aa2520a949b244a07d9
Brainisthekey <andriialieksieiev@gmail.com>
2021-04-21 18:03:54 +0200

    Revert "Linting done  - filenames: binance_clien.py and client.py"
    This reverts commit edd65e733037770e95f0c79cf1f8b1b62292b2a5.

--- HTTPclient/binance_client.py
+++ HTTPclient/binance_client.py
@@ -1,4 +1,4 @@
-from HTTPclient.client import HTTPClient
+from client import HTTPClient
 
 
 class BinanceClient(HTTPClient):
@@ -6,13 +6,8 @@ class BinanceClient(HTTPClient):
     apiKey = ''
     secretKey = ''
 
-    def __init__(self,
-                apiKey: str = None,
-                secretKey: str = None,
-                BASE_PATH: str = 'https://api.binance.com/api/v3/'
-                ):
+    def __init__(self, apiKey: str = None, secretKey: str = None, BASE_PATH: str = 'https://api.binance.com/api/v3/'):
         super().__init__(headers= {'apiKey' : apiKey, 'secretKey' : secretKey },
                          BASE_PATH=BASE_PATH)
         self.apiKey = apiKey
         self.secretKey = secretKey
-        
--- HTTPclient/client.py
+++ HTTPclient/client.py
@@ -1,23 +1,20 @@
 from enum import Enum
-from typing import List
 import requests
+from typing import List
+
 
 class HTTPClient():
     BASE_PATH = None
     headers = None
     suported_codes = None
 
-    def __init__(
-                self, headers: dict = None, suported_codes: List[int] = None, BASE_PATH: str = None
-                ):
+    def __init__(self, headers: dict = None, suported_codes: List[int] = None, BASE_PATH: str = None):
         self.headers = headers
         self.suported_codes = suported_codes
         self.BASE_PATH = BASE_PATH
         self.args = {'headers': self.headers}
 
-    def request(
-                self, type: Enum, path: str, params: dict = None, body: dict = None
-                ) -> requests.models.Response:
+    def request(self, type: Enum, path: str, params: dict = None, body: dict = None) -> requests.models.Response:
         self.args.update({'url': self.BASE_PATH + path})
         if type == HTTPClient.RequestType.GET:
             response = requests.get(**dict(self.args, params=params))
@@ -38,8 +35,9 @@ class HTTPClient():
         if self.suported_codes:
             if response.status_code in self.suported_codes:
                 return response
-            raise BaseException
-    
+            else:
+                raise BaseException
+        
     class RequestType(Enum):
         GET = 'GET'
         POST = 'POST'
