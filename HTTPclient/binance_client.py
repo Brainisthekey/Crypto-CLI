0ec19ea04edbe35eaa7445edc9bc6c6a2a203360
Brainisthekey <andriialieksieiev@gmail.com>
2021-04-21 18:48:08 +0200

    feat: last version formated client.py and binance_client.py

--- HTTPclient/binance_client.py
+++ HTTPclient/binance_client.py
@@ -1,4 +1,4 @@
-from client import HTTPClient
+from HTTPclient.client import HTTPClient
 
 
 class BinanceClient(HTTPClient):
@@ -6,8 +6,14 @@ class BinanceClient(HTTPClient):
     apiKey = ''
     secretKey = ''
 
-    def __init__(self, apiKey: str = None, secretKey: str = None, BASE_PATH: str = 'https://api.binance.com/api/v3/'):
-        super().__init__(headers= {'apiKey' : apiKey, 'secretKey' : secretKey },
-                         BASE_PATH=BASE_PATH)
+    def __init__(
+                self, apiKey: str = None, secretKey: str = None,
+                BASE_PATH: str = 'https://api.binance.com/api/v3/'
+                ):
+        super().__init__(
+                        headers= {'apiKey' : apiKey, 'secretKey' : secretKey },
+                         BASE_PATH=BASE_PATH
+                         )
         self.apiKey = apiKey
         self.secretKey = secretKey
+        
