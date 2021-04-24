b83fa1ea6693d1420367bd6d5593b2f8e5169244
Brainisthekey <andriialieksieiev@gmail.com>
2021-04-24 12:52:11 +0200

    feat: write tests for client.py, all passes

--- testing/test_HTTPclient.py
+++ testing/test_HTTPclient.py
@@ -4,12 +4,56 @@ from unittest import mock
 import pytest
 
 
-
-clientt = HTTPClient(BASE_PATH='https://api.binance.com/api/v3/', suported_codes=[404])
+client = HTTPClient(BASE_PATH='https://api.binance.com/api/v3/', suported_codes=[7777])
+#args = {'headers': 'headers'}
 
 @mock.patch('HTTPclient.client.requests.get')
-def test_request(mock_request_get, type=HTTPClient.RequestType.GET, path='1'):
-    mock_request_get.return_value = mock.Mock(**{'status_code' : 404, 'text.return_value' : "<html><body><h2>404 Not found</h2></body></html>"})
-    assert clientt.request(type=type, path=path) == mock_request_get(type=type, path=path).text
+def test_request_get(
+                mock_request_get,
+                type=HTTPClient.RequestType.GET,
+                path=''
+                ):
+    mock_request_get.return_value = mock.Mock(**{'status_code' : 7777})
+    assert client.request(type=type, path=path) == mock_request_get(type=type, path=path)
 
+@mock.patch('HTTPclient.client.requests.post')
+def test_request_post(
+                      mock_request_post,
+                      type=HTTPClient.RequestType.POST,
+                      path='',
+                      body={},
+                      params={}
+                    ):
+    mock_request_post.return_value = mock.Mock(**{'status_code' : 7777})
+    assert client.request(type=type, path=path, body=body, params=params) == mock_request_post(type=type, path=path, body=body, params=params)
 
+@mock.patch('HTTPclient.client.requests.put')
+def test_request_put(
+                      mock_request_put,
+                      type=HTTPClient.RequestType.PUT,
+                      path='',
+                      body={},
+                      params={}
+                    ):
+    mock_request_put.return_value = mock.Mock(**{'status_code' : 7777})
+    assert client.request(type=type, path=path, body=body, params=params) == mock_request_put(type=type, path=path, body=body, params=params)
+
+@mock.patch('HTTPclient.client.requests.delete')
+def test_request_delete(
+                        mock_request_delete,
+                        type=HTTPClient.RequestType.DELETE,
+                        path=''
+                        ):
+    mock_request_delete.return_value = mock.Mock(**{'status_code' : 7777})
+    assert client.request(type=type, path=path) == mock_request_delete(type=type, path=path)
+
+@mock.patch('HTTPclient.client.requests.patch')
+def test_request_patch(
+                      mock_request_patch,
+                      type=HTTPClient.RequestType.PATCH,
+                      path='',
+                      body={},
+                      params={}
+                    ):
+    mock_request_patch.return_value = mock.Mock(**{'status_code' : 7777})
+    assert client.request(type=type, path=path, body=body, params=params) == mock_request_patch(type=type, path=path, body=body, params=params)
