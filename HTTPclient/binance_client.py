03737e993864db477882fcc10f6143473a743204
Brainisthekey <andriialieksieiev@gmail.com>
2021-04-25 16:30:37 +0200

    feat: update params in binance_client.py

--- HTTPclient/binance_client.py
+++ HTTPclient/binance_client.py
@@ -3,16 +3,14 @@ from HTTPclient.client import HTTPClient
 
 class BinanceClient(HTTPClient):
 
-    apiKey = ''
-    secretKey = ''
-
     def __init__(
-                self, apiKey: str = None, secretKey: str = None,
+                self,
+                apiKey: str = None,
+                secretKey: str = None,
                 BASE_PATH: str = 'https://api.binance.com/api/v3/'
                 ):
+        
         super().__init__(
                         headers= {'apiKey' : apiKey, 'secretKey' : secretKey},
-                         BASE_PATH=BASE_PATH
-                         )
-        self.apiKey = apiKey
-        self.secretKey = secretKey
+                        BASE_PATH=BASE_PATH
+                        )
