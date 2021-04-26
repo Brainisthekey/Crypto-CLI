6529931291c041744bbf0b2bacebd58e9f302881
Brainisthekey <andriialieksieiev@gmail.com>
2021-04-26 13:27:58 +0200

    fear: load environment variables to project

--- tesing_requests_to_binance.py
+++ tesing_requests_to_binance.py
@@ -3,9 +3,13 @@ import hashlib
 import time
 from urllib.parse import urlencode
 import requests
+import os
+from dotenv import load_dotenv
 
-api_key = ""
-api_secret = ""
+load_dotenv()
+
+apiKey = os.environ.get('API_KEY')
+secretKey = os.environ.get('SECRET_KEY')
 
 request_url = "https://api.binance.com/api/v3/myTrades?"
 
@@ -16,11 +20,11 @@ querystring = urlencode({"timestamp": timestamp, "symbol": "BTCUSDT"})
 print(querystring)
 
 signature = hmac.new(
-    api_secret.encode("utf-8"), querystring.encode("utf-8"), hashlib.sha256
+    secretKey.encode("utf-8"), querystring.encode("utf-8"), hashlib.sha256
 ).hexdigest()
 print(signature)
 
 request_url += querystring + "&signature=" + signature
 
-r = requests.get(request_url, headers={"X-MBX-APIKEY": api_key})
+response = requests.get(request_url, headers={"X-MBX-APIKEY": apiKey})
 print(r.json()[30])
